ec7061d7a06420caff5dae651efd74b10bc3b937
Дмитрий Ширяев <dshiryaev@srt-llc.ru>
2023-10-05 13:15:19 +0400

    animation

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@ import { inputModule } from "./modules/input.js";
 import { tabsModule } from "./modules/tabs.js";
 import { menuModule } from "./modules/menu.js";
 import { scrollModule } from "./modules/scroll.js";
+import { animationModule } from "./modules/animation.js";
 
 inputModule();
 tabsModule();
 menuModule();
 scrollModule();
+animationModule();
---
+++ js/modules/animation.js
@@ -0,0 +1,25 @@
+export const animationModule = () => {
+    window.onload = () => {
+        const startedScreenElements = document.querySelectorAll('.home .anim');
+        const targetElements = document.querySelectorAll('.anim');
+    
+        const options = {
+            root: null,
+            rootMargin: '5px',
+            threshold: 0.5,
+        };
+
+        const callback = (entries, observer) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('animated');
+                }
+            });
+        };
+
+        const observer = new IntersectionObserver(callback, options);
+
+        targetElements.forEach(elem => observer.observe(elem));
+        startedScreenElements.forEach(elem => elem.classList.add('animated'));
+    };
+};
