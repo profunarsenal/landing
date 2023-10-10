e27d53696aaf2e075c339bf46367d6e9029d4d6d
Дмитрий Ширяев <dshiryaev@srt-llc.ru>
2023-10-10 19:27:32 +0400

    1

--- js/modules/tabs.js
+++ js/modules/tabs.js
@@ -1,55 +1,49 @@
 export const tabsModule = () => {
     const tabsButtons = document.querySelectorAll('.example__tab-button');
-    const desktopVideos = document.querySelectorAll('.example__video video');
     const desktopPlayButton = document.querySelector('.example__video .example__video-button');
     const mobilePlayButtons = document.querySelectorAll('.example__tab .example__video-button');
-    const mobileVideos = document.querySelectorAll('.example__tab-video video');
+    const allButtons = [desktopPlayButton, ...mobilePlayButtons];
+    
+    const createVideo = (button) => {
+        const activeTab = document.querySelector('.example__tabs .active');
+        const tabId = activeTab.getAttribute('id');
+        const videoBlock = button.parentElement;
+        const video = document.createElement('video');
+        const source = document.createElement('source');
 
-    const pauseVideos = (videos, button) => {
-        videos.forEach((video) => {
-            video.classList.remove('play');
-            video.pause();
-        });
+        video.setAttribute('controls', '');
+        video.classList.add('example-video');
+        source.setAttribute('src', `./assets/video/${tabId}.mov`);
 
-        button.classList.remove('play');
+        video.append(source);
+        videoBlock.insertBefore(video, button);
+        button.classList.add('play');
+        video.play();
     };
 
     tabsButtons.forEach((tabButton) => {
         tabButton.addEventListener('click', () => {
+            const video = document.querySelector('.example-video');
+
             tabsButtons.forEach((button) => {
                 button.parentElement.classList.remove('active');
             });
 
             tabButton.parentElement.classList.add('active');
-
-            pauseVideos(desktopVideos, desktopPlayButton);
-            pauseVideos(mobileVideos, tabButton.parentElement.querySelector('.example__video-button'));
-        });
-    });
-
-    desktopPlayButton.addEventListener('click', () => {
-        const activeTab = document.querySelector('.example__tabs .active');
-        const tabId = activeTab.getAttribute('id');
-
-        desktopVideos.forEach((video) => {
-            const videoId = video.getAttribute('id');
-
-            if (videoId === `video-${tabId}`) {
-                video.classList.add('play');
-                desktopPlayButton.classList.add('play');
-                video.play();
-            } else {
-                video.classList.remove('play');
+            
+            if (video) {
+                video.remove();
             }
+            
+            allButtons.forEach((button) => {
+                button.classList.remove('play');
+            });
         });
     });
 
-    mobilePlayButtons.forEach((button) => {
+    allButtons.forEach((button) => {
         button.addEventListener('click', () => {
-            const video = button.parentElement.querySelector('video');
-            video.classList.add('play');
-            button.classList.add('play');
-            video.play();
+            createVideo(button);
         });
     });
 };
